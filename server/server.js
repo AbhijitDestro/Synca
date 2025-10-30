5568f9f7304021ca0baef3a5a9c86db286803c17
AbhijitDestro <abhijitdutta1806@gmail.com>
2025-10-30 23:15:16 +0530

    implemented all the inngest functions and authMiddleware

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ import cors from 'cors'
 import { clerkMiddleware } from '@clerk/express'
 import {serve} from "inngest/express"
 import {inngest,functions} from "./inngest/index.js"
+import requireAuth from "./middleware/authMiddleware.js"
+import workspaceRouter from "./routes/workspaceRoutes.js"
 
 const app=express()
 
@@ -12,8 +14,8 @@ app.use(cors())
 app.use(clerkMiddleware())
 
 app.get("/",(req,res)=>{res.send("<h1>Server is live!!</h1>")})
-
 app.use("/api/inngest",serve({client:inngest,functions}))
+app.use("/api/workspaces",requireAuth, workspaceRouter)
 
 // Error handling middleware
 app.use((err, req, res, next) => {
